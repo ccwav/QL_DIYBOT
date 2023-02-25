675bf303f623a0edaaf29cca00a1945c01303c5d
ccwav <ccwav@163.com>
2023-02-25 23:04:31 +0800

    解析口令 update

--- jbot/user/jxjd_Global.py
+++ jbot/user/jxjd_Global.py
@@ -1,27 +1,16 @@
 from telethon import events
-from .login import user
-
 from .. import jdbot
 from ..diy.utils import read, write
 import re
-import requests
+import requests,json
+
+try:
+    from .login import user
+except:
+    from .. import user
+    
 @user.on(events.NewMessage(pattern=r'^jx', outgoing=True))
 async def jcmd(event):
-    configs = read("str")    
-    M_API_TOKEN = ""
-    if "M_API_TOKEN" in configs:
-        TempConfigs = configs.split("\n")
-        for config in TempConfigs:
-            if "M_API_TOKEN" not in config:                
-                continue            
-            kv = config.replace("export ", "")
-            M_API_TOKEN = re.findall(r'"([^"]*)"', kv)[0]
-            
-    if len(M_API_TOKEN) == 0:
-        await user.send_message(event.chat_id,"请先找 @magic_noyify_bot 申请解析token填入青龙变量,关键字为M_API_TOKEN")
-        return 
-            
-    headers = {"token": M_API_TOKEN}
     strText=""
     if event.is_reply is True:
         reply = await event.get_reply_message()
@@ -33,17 +22,29 @@ async def jcmd(event):
     
     if strText==None:
         await user.send_message(event.chat_id,'请指定要解析的口令,格式: jx 口令 或对口令直接回复jx ')
-        return    
+        return 
         
-    data = requests.post("http://ailoveu.eu.org:19840/jCommand",
-                             headers=headers,
-                             json={"code": strText}).json()
-    code = data.get("code")
-    if code == 200:
-        data = data["data"]
-        title = data["title"]
-        jump_url = data["jumpUrl"]
-        await user.send_message(event.chat_id,title+"\n"+jump_url)
+    jumpUrl=""
+    title=""    
+    jiexiurl = "http://api.nolanstore.top/JComExchange"    
+    data ={"code": strText}
+    headers={"Content-Type": "application/json"}
+    issuccess=False
+    for num in range(10):  
+        try:
+            res=requests.post(url=jiexiurl,headers=headers,json=data,timeout=3)
+            issuccess=True
+        except:
+            issuccess=False
+        if issuccess:
+            break
+    resdata=json.loads(res.text)
+    if resdata["code"]=="0":  
+        title = resdata["data"]['title']
+        jumpUrl = resdata["data"]['jumpUrl']
+        
+    if jumpUrl != "":        
+        await user.send_message(event.chat_id,title+"\n"+jumpUrl)
     else:
         await user.send_message(event.chat_id,"解析出错:"+data.get("data"))
     
